cbb6b400866ef529d13013c1943a87bb03085a12
Megha-P-Varghese <142739312+Megha-P-Varghese@users.noreply.github.com>
2024-07-24 22:36:12 +0530

    Add files via upload

---
+++ polymorphism.py
@@ -0,0 +1,18 @@
+class india:
+    def capital(self):
+        print("Delhi is capital of india")
+
+class kerala:
+    def capital(self):
+        print("Trivandrum is the capital of kerala")
+
+class tamilnadu:
+    def capital(self):
+        print("Chennai is the capital of tamilnadu")
+    
+obj1=india()
+obj2=kerala()
+obj3=tamilnadu()
+obj1.capital()
+obj2.capital()
+obj3.capital()
