839ff64a796ab742c3046b7bc7ecf1dd8e7e475c
Megha-P-Varghese <142739312+Megha-P-Varghese@users.noreply.github.com>
2024-07-24 22:37:24 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+print('no 1:')
+x=int(input())
+print('no 2:')
+y=int(input())  
+print('+\n -\n *\n /\n' )
+a=input()
+if a=='+' :
+    print(x+y) 
+elif a=='-' :
+    print(x-y)
+elif a=='*' :
+    print(x*y)
+elif a=='/' :
+    print(x/y)
